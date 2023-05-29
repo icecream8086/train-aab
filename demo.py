8369a7260839e0579d9e0e5521d115e0f54f43f9
icecream8086 <icecream8086@outlook.com>
2023-05-29 16:01:23 +0800

    添加接口的功能

--- demo.py
+++ demo.py
@@ -1,13 +1,29 @@
-from sdk.access import predict_image
+from sdk.access import predict_image,predict_images
 
 #sdk用法
+
+# 简化版
+# predict_image('test_image.jpg')
 predicted = predict_image('test_image.jpg')
 print('Predicted label: {}'.format(predicted))
 
+# # 返回
+# # default mode cuda
+# # Predicted label: Tomato_Leaf_Spot_Disease
 
-predicted = predict_image('apple.jpg','a.pth')
+
+# 复杂版 
+#
+# def predict_images(image_path, model_path='a.pth', device='cpu', batch_size=1):
+# 允许指定
+#  *默认模型
+#  *设备类型
+#  *处理图片的批量大小(实验型功能,理论上比起单队列模式可以获得更优秀的加速比,但是调用不当可能造成额外的资源消耗)
+#
+
+predicted = predict_images('apple.jpg','a.pth','cpu',1)
 print('Predicted label: {}'.format(predicted))
 
 # # 返回
-# # mode cuda
+# # default mode cuda
 # # Predicted label: Tomato_Leaf_Spot_Disease
--- sdk/access.py
+++ sdk/access.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 # Check if GPU is available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"mode {device}")
+print(f"default mode {device}")
 
 # Define data transformations
 transform = transforms.Compose([
@@ -81,3 +81,52 @@ def predict_image(image_path, model_path='a.pth'):
 
     # Return the predicted label
     return predicted_label
+
+def predict_images(image_path, model_path='a.pth', device='cpu', batch_size=1):
+    
+    # batch_size指定在一个模型迭代中应该处理多少张图片
+    # 通过设置大于1的 batch_size，您可以在同一次模型正向传递中同时处理多个图像，这比逐个处理每个图像更快。
+    # 最好不要乱动batch_size,可能有奇怪的bug
+
+    # Load the model
+    model = Net()
+
+    # Move the model to the device
+    model.to(device)
+
+    # Load the model state dictionary
+    state_dict = torch.load(model_path)
+
+    # Load the state dictionary to the model
+    model.load_state_dict(state_dict)
+
+    # Set the model to evaluation mode
+    model.eval()
+
+    # Load the image
+    image = Image.open(image_path)
+
+    # Apply transformations to the image
+    image_tensor = transform(image)
+    image_tensor = image_tensor.unsqueeze(0) # Add a batch dimension
+
+    # If batch size is greater than 1, repeat the image tensor
+    if batch_size > 1:
+        image_tensor = image_tensor.repeat(batch_size, 1, 1, 1)
+
+    # Move the data to the device
+    image_tensor = image_tensor.to(device)
+
+    # Use the model to make a prediction
+    with torch.no_grad():
+        output = model(image_tensor)
+        _, predicted = torch.max(output.data, 1)
+        classes = train_set_1.classes
+        predicted_labels = [classes[pred] for pred in predicted]
+
+    # If batch size is 1, return the predicted label as a string
+    if batch_size == 1:
+        return predicted_labels[0]
+    # If batch size is greater than 1, return the predicted labels as a list of strings
+    else:
+        return predicted_labels
