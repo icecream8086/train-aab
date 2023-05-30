01ba739d59a5266996bf376b327f3dc1562206f6
icecream8086 <icecream8086@outlook.com>
2023-05-30 08:41:29 +0800

    2

--- ld.py
+++ ld.py
@@ -4,14 +4,21 @@ from torch import nn
 from torchvision import datasets, transforms
 from CNN_lib.net_model import Net
 
-
+mean = [0.5, 0.5, 0.5]
+std = [0.5, 0.5, 0.5]
 # 定义数据预处理的方法
+
 transform = transforms.Compose([
     transforms.Resize(256),
-    transforms.CenterCrop(224),
+    transforms.RandomRotation(30),
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.Normalize(mean=mean, std=std)
 ])
+
+
 # 加载数据集
 train_set_1 = datasets.ImageFolder('dataset', transform=transform)
 
